937d0742fd6b18c850a83edbb9d4df7bac77f2f2
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-12-04 21:37:35 +0800

    Update ASSIGNMENT 7_PROG 2_PASSWORD VALIDATOR.py
    Relocated the userInput inside the while-loop so that the loop is inside the def function unlike the first commit. Also added a user greeting and the criteria.

--- ASSIGNMENT 7_PROG 2_PASSWORD VALIDATOR.py
+++ ASSIGNMENT 7_PROG 2_PASSWORD VALIDATOR.py
@@ -1,6 +1,4 @@
-import time, sys, random, colorama
-from colorama import Fore, Style
-colorama.init (autoreset=True)
+import time, sys, random
 typing_speed = 50
 
 def pause(str):
@@ -15,43 +13,47 @@ def pauseNext(str):
         sys.stdout.flush()
         time.sleep(random.random()*10.0/typing_speed)
 
+def greetUser():
+    pauseNext("\nWelcome to Password Validator!")
+    time.sleep(2)
+    print("\nTo ensure the validity of your password, please make sure to meet the following criteria:\n")
+    time.sleep(2)
+    pause("Password must be greater than 15 characters.\n")
+    time.sleep(.5)
+    pause("Password must contain at least one number.\n")
+    time.sleep(.5)
+    pause("Password must contain at least one capital letter.\n")
+    time.sleep(.5)
+    pause("Password must contain at least one special character.\n")
+    time.sleep(1)
+
 def checkPassword():
-    userInput = input("\nEnter your password: ")
-    print("\n")
-    numbers = "[0123456789]"
-    uppercase = "[ABCDEFGHIJKLMNOPQRSTUVWXYZ]"
-    specialChars = "[!#$%&()*+,-./:;<=>[]?@\^_{|}~]"
-    criteria = 0; x = 0; y = 0; z = 0
-    while criteria != 3:
-        while len(userInput) < 15:
-            print("Your password must be greater than 15 characters.")
-            break
-        if not any(char in numbers for char in userInput):
-            print("Your password must contain at least one number.")
-            break
-        elif not any(char in uppercase for char in userInput):
-            print("Password must contain at least one uppercase letter.")
-            break
-        elif not any(char in specialChars for char in userInput):
-            print("Password must contain at least one special character.")
-            break
+    criteria = {
+        "number" : "[0123456789]",
+        "capital letter" : "[ABCDEFGHIJKLMNOPQRSTUVWXYZ]",
+        "special character" : """[!#$%&"'()*+,-./:;<=>[]?@\^_{|}~]""",
+    }
+    checkCriteria = 0
+    userInput = ''
+    while len(userInput) < 15 or checkCriteria != 3:
+        userInput = input("\nEnter new password: ")
+        checkCriteria = 0
+        for nameCriterion, criterion in criteria.items():
+            pauseNext(f"\nChecking {nameCriterion}...\n")
+            valid = [ch for ch in criterion if ch in userInput]
+            if valid: 
+                checkCriteria += 1
+            else:
+                time.sleep(.5)
+                print(f"Your password must contain at least one {nameCriterion}.")
+                time.sleep(.5)
+        if checkCriteria == 3:
+            time.sleep(1)
+            print ("\nPassword is valid. You may proceed.")
+            time.sleep(1)
+        else:
+            time.sleep(1) 
+            print("\nPassword is invalid. Please try again.")   
 
-        for i in range(len(userInput)):
-            if userInput[i] in numbers:
-                x = 1
-            elif userInput[i] in uppercase:
-                y = 1
-            elif userInput[i] in specialChars:
-                z = 1
-            criteria = x + y + z
-    if criteria != 3:
-        print("\nPassword is invalid. Please try again. ")
-        return False
-    else:
-        print("Password is valid. You may proceed.")
-        return True
-        
-valid = False
-while not valid:
-    valid = checkPassword()
-
+greetUser()
+checkPassword()
